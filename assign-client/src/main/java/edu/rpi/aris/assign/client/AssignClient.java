6891aa4b5895d51dc5668c39a953e845cb6c4010
Logan Garber <logangarber@hotmail.com>
2018-09-02 15:13:27 -0400

    Fixed java compiler not using UTF-8 on windows

--- assign-client/src/main/java/edu/rpi/aris/assign/client/AssignClient.java
+++ assign-client/src/main/java/edu/rpi/aris/assign/client/AssignClient.java
@@ -51,6 +51,7 @@ public class AssignClient extends Application implements ArisExceptionHandler {
     public static void main(String[] args) throws IOException {
         LibAssign.setLogLocation(new File(Config.CLIENT_STORAGE_DIR, "logs"));
         LibAssign.initModuleService(Config.CLIENT_MODULES_DIR, false);
+        logger.info("Starting Assign Client with Java " + SystemUtils.JAVA_RUNTIME_VERSION);
         logger.info("Loaded client modules: " + ModuleService.getService().moduleNames());
         LibAssign.getInstance().init(false, args, new MainCallbackListener() {
             @Override
