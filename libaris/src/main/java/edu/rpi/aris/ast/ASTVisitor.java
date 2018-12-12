b2afd7cd4577164fc3344b3e24337535efd8102e
Avi Weinstock <aweinstock314@gmail.com>
2018-12-12 15:28:04 -0500

    Add ASTVisitor for ast.Expression.

---
+++ libaris/src/main/java/edu/rpi/aris/ast/ASTVisitor.java
@@ -0,0 +1,23 @@
+package edu.rpi.aris.ast;
+
+import edu.rpi.aris.ast.Expression.*;
+import java.util.*;
+
+public interface ASTVisitor<T> {
+    public T visitExpression(Expression e);
+    public T visitBottomExpression(BottomExpression e);
+    public T visitPredicateExpression(PredicateExpression e);
+    public T visitUnaryExpression(UnaryExpression e);
+    public T visitNotExpression(NotExpression e);
+    public T visitBinaryExpression(BinaryExpression e);
+    public T visitImplicationExpression(ImplicationExpression e);
+    public T visitAddExpression(AddExpression e);
+    public T visitMultExpression(MultExpression e);
+    public T visitAssociativeBinopExpression(AssociativeBinopExpression e);
+    public T visitAndExpression(AndExpression e);
+    public T visitOrExpression(OrExpression e);
+    public T visitBiconExpression(BiconExpression e);
+    public T visitQuantifierExpression(QuantifierExpression e);
+    public T visitForallExpression(ForallExpression e);
+    public T visitExistsExpression(ExistsExpression e);
+}
