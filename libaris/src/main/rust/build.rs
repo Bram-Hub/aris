d3fedbd9bbe7c6018ee7494320a2f91fd2a7dc3e
Plailect <plailect@pm.me>
2020-03-03 20:16:51 -0500

    workaround rust-lang/rls-vscode#586

--- libaris/src/main/rust/build.rs
+++ libaris/src/main/rust/build.rs
@@ -3,15 +3,7 @@ extern crate version_check;
 
 use std::env;
 
-fn main() {
-    // generate_docstrings_from_macros is an internal detail of 
-    // define_rewrite_rule, to make it only generate documentation on versions of rust 
-    // in which it is legal for a macro invocation to be captured by an expr metavariable
-    // https://blog.rust-lang.org/2019/12/19/Rust-1.40.0.html#macro-and-attribute-improvements
-    if version_check::is_min_version("1.40.0").unwrap_or(false) {
-        println!("cargo:rustc-cfg=generate_docstrings_from_macros");
-    }
-
+fn write_headers() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
     cbindgen::Builder::new()
@@ -21,4 +13,18 @@ fn main() {
         .generate()
         .expect("Unable to generate bindings")
         .write_to_file("aris.h");
+}
+
+fn main() {
+    // generate_docstrings_from_macros is an internal detail of
+    // define_rewrite_rule, to make it only generate documentation on versions of rust
+    // in which it is legal for a macro invocation to be captured by an expr metavariable
+    // https://blog.rust-lang.org/2019/12/19/Rust-1.40.0.html#macro-and-attribute-improvements
+    if version_check::is_min_version("1.40.0").unwrap_or(false) {
+        println!("cargo:rustc-cfg=generate_docstrings_from_macros");
+    }
+
+    // https://github.com/rust-lang/rls-vscode/issues/586
+    #[cfg(not(feature="workaround_rls_vscode_bug_586"))]
+    write_headers();
 }
