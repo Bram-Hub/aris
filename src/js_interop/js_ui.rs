8bce3a4b6203a534e51c4f170042c76eefa4832b
Avi Weinstock <aweinstock314@gmail.com>
2020-03-09 19:36:11 -0400

    In the JS UI, separate out ExprEntry into it's own component.

--- src/js_interop/js_ui.rs
+++ src/js_interop/js_ui.rs
@@ -2,15 +2,58 @@ extern crate yew;
 use yew::prelude::*;
 use expression::Expr;
 
+pub struct ExprEntry {
+    link: ComponentLink<Self>,
+    current_input: String,
+    last_good_parse: String,
+    current_expr: Option<Expr>,
+    onchange: Callback<(String, Option<Expr>)>,
+}
+
+#[derive(Clone, Properties)]
+pub struct ExprEntryProps {
+    pub initial_contents: String,
+    pub onchange: Callback<(String, Option<Expr>)>,
+}
+
+impl Component for ExprEntry {
+    type Message = String;
+    type Properties = ExprEntryProps;
+    fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        let mut ret = Self {
+            link,
+            current_expr: None,
+            current_input: props.initial_contents.clone(),
+            last_good_parse: "".into(),
+            onchange: props.onchange,
+        };
+        ret.update(props.initial_contents);
+        ret
+    }
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        use parser::parse;
+        self.current_expr = parse(&*msg);
+        if let Some(expr) = &self.current_expr {
+            self.last_good_parse = format!("{}", expr);
+        }
+        self.onchange.emit((self.last_good_parse.clone(), self.current_expr.clone()));
+        true
+    }
+    fn view(&self) -> Html {
+        html! {
+            <input type="text" oninput=self.link.callback(|e: InputData| e.value) style="width:400px" value={ &self.current_input } />
+        }
+    }
+}
+
 pub struct App {
     link: ComponentLink<Self>,
-    string_expr: String,
     last_good_parse: String,
-    test_expr: Option<Expr>
+    current_expr: Option<Expr>,
 }
 
 pub enum Msg {
-    TestExpression(String),
+    ExprChanged(String, Option<Expr>),
 }
 
 impl Component for App {
@@ -18,26 +61,14 @@ impl Component for App {
     type Properties = ();
 
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        use parser::parse;
-        let initial_expr = "forall A, ((exists B, A -> B) & C & f(x, y | z)) <-> Q <-> R";
-        let test_expr = parse(initial_expr);
-        let last_good_parse = format!("{}", test_expr.as_ref().unwrap());
-        Self {
-            link,
-            string_expr: initial_expr.into(),
-            test_expr,
-            last_good_parse,
-        }
+        Self { link, last_good_parse: "".into(), current_expr: None }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::TestExpression(data) => {
-                use parser::parse;
-                self.test_expr = parse(&*data);
-                if let Some(expr) = &self.test_expr {
-                    self.last_good_parse = format!("{}", expr);
-                }
+            Msg::ExprChanged(last_good_parse, current_expr) => {
+                self.last_good_parse = last_good_parse;
+                self.current_expr = current_expr;
                 true
             },
         }
@@ -47,12 +78,12 @@ impl Component for App {
         html! {
             <div>
                 <p>{ "Enter Expression:" }</p>
-                <input type="text" oninput=self.link.callback(|e: InputData| Msg::TestExpression(e.value)) style="width:400px" value={ &self.string_expr } />
+                <ExprEntry initial_contents="forall A, ((exists B, A -> B) & C & f(x, y | z)) <-> Q <-> R" onchange=self.link.callback(|(x, y)| Msg::ExprChanged(x, y)) />
                 <div>
                     { &self.last_good_parse }
                     <br/>
                     <pre>
-                        { self.test_expr.as_ref().map(|e| format!("{:#?}", e)).unwrap_or("Error".into()) }
+                        { self.current_expr.as_ref().map(|e| format!("{:#?}", e)).unwrap_or("Error".into()) }
                     </pre>
                 </div>
             </div>
