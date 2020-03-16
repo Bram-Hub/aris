7439e87d1f3efd261d5651d35a150f066243e779
aweinstock314 <aweinstock314@gmail.com>
2020-03-16 23:50:24 +0000

    Deploying to gh-pages from  - e839134f7f36ca12cc3d3e19e57aad78f8bcd58d 🚀

--- pkg/libaris.js
+++ pkg/libaris.js
@@ -560,7 +560,7 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper689 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper702 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 232, __wbg_adapter_16);
         return addHeapObject(ret);
     };
