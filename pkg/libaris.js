5f82f0bc185820a74159ea6172e5993a97f69b6d
aweinstock314 <aweinstock314@gmail.com>
2020-03-16 21:36:22 +0000

    Deploying to gh-pages from  - 580c0689834c90e3b01fdc14da718009afb9a8d9 🚀

--- pkg/libaris.js
+++ pkg/libaris.js
@@ -560,8 +560,8 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper436 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 172, __wbg_adapter_16);
+    imports.wbg.__wbindgen_closure_wrapper674 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 227, __wbg_adapter_16);
         return addHeapObject(ret);
     };
 
--- pkg/libaris_bg.d.ts
+++ pkg/libaris_bg.d.ts
@@ -1,11 +1,11 @@
 /* tslint:disable */
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
+export function run_app(): void;
 export function aris_vec_expr_index(a: number, b: number, c: number): void;
 export function aris_vec_string_index(a: number, b: number, c: number): void;
 export function aris_box_expr_deref(a: number, b: number): void;
 export function aris_expr_parse(a: number): number;
-export function run_app(): void;
 export function __wbindgen_malloc(a: number): number;
 export function __wbindgen_realloc(a: number, b: number, c: number): number;
 export const __wbindgen_export_2: WebAssembly.Table;
