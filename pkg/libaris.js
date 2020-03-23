537a591a455cdd7d9eb48fd47cb688f35a080d79
aweinstock314 <aweinstock314@gmail.com>
2020-03-23 04:10:32 +0000

    Deploying to gh-pages from  - 5961856d45068e3c00a53fbefd6c0951497e8676 🚀

--- pkg/libaris.js
+++ pkg/libaris.js
@@ -265,10 +265,6 @@ async function init(input) {
     imports.wbg.__wbindgen_object_drop_ref = function(arg0) {
         takeObject(arg0);
     };
-    imports.wbg.__wbindgen_object_clone_ref = function(arg0) {
-        var ret = getObject(arg0);
-        return addHeapObject(ret);
-    };
     imports.wbg.__wbindgen_cb_drop = function(arg0) {
         const obj = takeObject(arg0).original;
         if (obj.cnt-- == 1) {
@@ -277,6 +273,10 @@ async function init(input) {
         }
         var ret = false;
         return ret;
+    };
+    imports.wbg.__wbindgen_object_clone_ref = function(arg0) {
+        var ret = getObject(arg0);
+        return addHeapObject(ret);
     };
     imports.wbg.__wbindgen_string_new = function(arg0, arg1) {
         var ret = getStringFromWasm0(arg0, arg1);
@@ -560,8 +560,8 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper927 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 283, __wbg_adapter_16);
+    imports.wbg.__wbindgen_closure_wrapper961 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 303, __wbg_adapter_16);
         return addHeapObject(ret);
     };
 
