fac09836af8971820b7a9b17e55f7858786553b5
aweinstock314 <aweinstock314@gmail.com>
2020-03-25 18:44:29 +0000

    Deploying to gh-pages from  - 933437fe8e03fbc1e9717436e833c74eeb080e54 🚀

--- pkg/libaris_bg.d.ts
+++ pkg/libaris_bg.d.ts
@@ -1,16 +1,16 @@
 /* tslint:disable */
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
+export function run_app(): void;
 export function aris_vec_expr_index(a: number, b: number, c: number): void;
 export function aris_vec_string_index(a: number, b: number, c: number): void;
 export function aris_box_expr_deref(a: number, b: number): void;
 export function aris_expr_parse(a: number): number;
-export function run_app(): void;
 export function __wbindgen_malloc(a: number): number;
 export function __wbindgen_realloc(a: number, b: number, c: number): number;
 export const __wbindgen_export_2: WebAssembly.Table;
-export function _dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h893c794a6ef7fcc0(a: number, b: number, c: number): void;
-export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h06183e758dc95323(a: number, b: number, c: number): void;
 export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h1277b4d55f52d8fa(a: number, b: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h06183e758dc95323(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h893c794a6ef7fcc0(a: number, b: number, c: number): void;
 export function __wbindgen_free(a: number, b: number): void;
 export function __wbindgen_exn_store(a: number): void;
