4b1d693cc50336705ff90caf9ed0add00b55ff7f
aweinstock314 <aweinstock314@gmail.com>
2020-06-11 10:26:41 +0000

    Deploying to gh-pages from  @ d5764731206b7b0be75c24f79df6cae2747a480e 🚀

--- pkg/libaris.js
+++ pkg/libaris.js
@@ -662,16 +662,16 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper190 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 3, __wbg_adapter_20);
+    imports.wbg.__wbindgen_closure_wrapper300 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 40, __wbg_adapter_20);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper192 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 3, __wbg_adapter_23);
+    imports.wbg.__wbindgen_closure_wrapper302 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 40, __wbg_adapter_23);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper1174 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 360, __wbg_adapter_26);
+    imports.wbg.__wbindgen_closure_wrapper1465 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 450, __wbg_adapter_26);
         return addHeapObject(ret);
     };
 
