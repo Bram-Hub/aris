e2ccaa3bbf85a83bde09db89b14cd079b72ba33f
Benjamin Levy <blevy@protonmail.com>
2020-07-05 00:17:51 -0400

    Style ExprAstWidget with Bootstrap

--- web-app/src/components/expr_ast_widget.rs
+++ web-app/src/components/expr_ast_widget.rs
@@ -43,22 +43,37 @@ impl Component for ExprAstWidget {
         true
     }
     fn view(&self) -> Html {
+        // Convert expression to debug string
         let expr_debug = self
             .current_expr
             .as_ref()
-            .map(|e| format!("{:#?}", e))
-            .unwrap_or("Error".into());
+            .map(|e| format!("{:#?}", e));
+
+        // Convert debug expression to HTML or parse error
+        let expr_debug = match expr_debug {
+            Some(s) => {
+                html! {
+                    <div class="card">
+                        <pre> { s } </pre>
+                    </div>
+                }
+            }
+            None => {
+                html! {
+                    <div class="alert alert-danger"> { "Parse error" } </div>
+                }
+            }
+        };
+
         html! {
-            <div>
-                <h2> {"Enter Expression:"} </h2>
+            <div class="alert alert-primary m-4">
+                <h2> { "Enter Expression:" } </h2>
                 <ExprEntry
                     oninput=self.link.callback(|value| value)
                     init_value={ &self.current_input } />
-                <div>
-                    { &self.last_good_parse }
-                    <br/>
-                    <pre> { expr_debug } </pre>
-                </div>
+                <hr />
+                <h5> { &self.last_good_parse } </h5>
+                { expr_debug }
             </div>
         }
     }
