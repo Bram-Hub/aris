e41aafc344fbd2f8df443256175644c662d4a699
Benjamin Levy <blevy@protonmail.com>
2020-09-09 21:20:06 -0400

    Fix C bindings build warning

--- bindings/c/src/c_expression.rs
+++ bindings/c/src/c_expression.rs
@@ -5,19 +5,6 @@ pub use aris::expr::Expr;
 use std::ffi::CStr;
 
 #[no_mangle]
-pub extern "C" fn aris_vec_expr_index(x: &Vec<Expr>, i: usize) -> Expr {
-    x[i].clone()
-}
-#[no_mangle]
-pub extern "C" fn aris_vec_string_index(x: &Vec<String>, i: usize) -> String {
-    x[i].clone()
-}
-#[no_mangle]
-pub extern "C" fn aris_box_expr_deref(x: &Box<Expr>) -> Expr {
-    *x.clone()
-}
-
-#[no_mangle]
 pub extern "C" fn aris_expr_parse(e: *const i8) -> *mut Expr {
     with_null_options(|| {
         let s = unsafe { CStr::from_ptr(e) }.to_string_lossy().into_owned();
