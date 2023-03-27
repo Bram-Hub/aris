49e99246709f0dae4bb31ccfb4df989cd523c74f
io12 <7348004+io12@users.noreply.github.com>
2023-03-27 22:23:32 +0000

    Deploying to gh-pages from  @ c1a41d447ed1c791efa3f93892048b3005df454e 🚀

--- pkg/aris_web_app.js
+++ pkg/aris_web_app.js
@@ -776,15 +776,15 @@ function getImports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper966 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper967 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 347, __wbg_adapter_18);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper968 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper969 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 347, __wbg_adapter_21);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper970 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper971 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 347, __wbg_adapter_24);
         return addHeapObject(ret);
     };
