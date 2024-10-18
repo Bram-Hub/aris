ad2b0c52d39a65c5fd09da458ad45319ba36a103
Zachary Bonagura <zachbonagura@gmail.com>
2024-10-18 18:12:26 -0400

    Revert "Fixed formatting errors, updated packages to most current version"
    This reverts commit 364572298cc1dcae1f6a4a9fde925abd06f1affa.

--- bindings/java/src/lib.rs
+++ bindings/java/src/lib.rs
@@ -26,7 +26,6 @@ pub fn java_iterator_for_each<F: FnMut(JObject) -> jni::errors::Result<()>>(env:
 
 /// Wraps a Rust function, converting both Result::Err and panic into instances of Java's RuntimeException.
 /// Please use this on all native methods, otherwise a Rust panic/unwrap will crash the Java UI instead of popping a dialog box with the message.
-#[allow(deprecated)]
 pub fn with_thrown_errors<A, F: FnOnce(&JNIEnv) -> jni::errors::Result<A> + UnwindSafe>(env: &JNIEnv, f: F) -> A {
     use std::panic::{set_hook, take_hook, PanicInfo};
     let old_hook = take_hook();
