a91d30cb9d9cc68d8b69316ae877fb9cc8395bb3
etbala <76022348+etbala@users.noreply.github.com>
2025-04-18 08:40:40 +0000

    Deploying to gh-pages from  @ b274a4bace0e44bb16b905d95744584b2d67a644 🚀

--- pkg/aris_web_app.js
+++ pkg/aris_web_app.js
@@ -714,15 +714,15 @@ function __wbg_get_imports() {
         const ret = false;
         return ret;
     };
-    imports.wbg.__wbindgen_closure_wrapper2175 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper2185 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 632, __wbg_adapter_22);
         return ret;
     };
-    imports.wbg.__wbindgen_closure_wrapper2179 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper2189 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 636, __wbg_adapter_25);
         return ret;
     };
-    imports.wbg.__wbindgen_closure_wrapper2204 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper2214 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 648, __wbg_adapter_28);
         return ret;
     };
